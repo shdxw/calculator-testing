cd93cd1e9d65485b48d1a50b08a9b9ffdd511672
vlader <Vladarscb@gmail.com>
2021-04-06 22:36:54 +1000

    simple tests for floats and integers

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,27 +1,29 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import jdk.jfr.Description;
 import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.MainPage;
 
 public class SampleTest {
 
-    private AndroidDriver<AndroidElement> driver;
-    private MainPage mainPage;
+    private static AndroidDriver<AndroidElement> driver;
+    private static MainPage mainPage;
 
-    @Before
-    public void setUp() throws MalformedURLException {
+    @BeforeClass
+    public static void setUp() throws MalformedURLException {
         Properties cfg = new Properties();
         try (FileInputStream in =
                      new FileInputStream("src/test/resources/test.properties")) {
@@ -41,14 +43,162 @@ public class SampleTest {
         driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
         mainPage = new MainPage(driver);
     }
-
-    @Test
-    public void sampleTest() {
-        System.out.println(mainPage.keyPlus.getId());;
+    @Before
+    public void reset() {
+        mainPage.keyReset.click();
     }
 
-    @After
-    public void tearDown() {
+    @Ignore
+    @Test
+    public void intsPlusTest() {
+        List<Integer> integers = Arrays.asList(1, 20, 300, 4000, 5000);
+        for (int i = 0; i < integers.size(); i++) {
+                mainPage.inputFieldLeft.setValue(integers.get(i).toString());
+                mainPage.inputFieldRight.setValue(integers.get(integers.size() - 1 - i).toString());
+                mainPage.keyPlus.click();
+                String v = mainPage.result.getText();
+                String[] res = v.split(" ");
+                v = res[res.length - 1];
+                String expected = String.format("%d,00", integers.get(i) + integers.get(integers.size() - 1 - i));
+                Assert.assertEquals(expected, v);
+                reset();
+        }
+    }
+
+    /*
+    *выкидывает ошибку при отрицательном результате
+    * */
+    @Ignore
+    @Test
+    public void intsMinusTest() {
+        List<Integer> integers = Arrays.asList(1, 20, 300, 4000, 5000);
+        for (int i = 0; i < integers.size(); i++) {
+            mainPage.inputFieldLeft.setValue(integers.get(i).toString());
+            mainPage.inputFieldRight.setValue(integers.get(integers.size() - 1 - i).toString());
+            mainPage.keyMinus.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            String expected = String.format("%d,00", integers.get(i) - integers.get(integers.size() - 1 - i));
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void intsMulTest() {
+        List<Integer> integers = Arrays.asList(1, 20, 300, 4000, 5000);
+        for (int i = 0; i < integers.size(); i++) {
+            mainPage.inputFieldLeft.setValue(integers.get(i).toString());
+            mainPage.inputFieldRight.setValue(integers.get(integers.size() - 1 - i).toString());
+            mainPage.keyMul.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            String expected = String.format("%d,00", integers.get(i) * integers.get(integers.size() - 1 - i));
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void intsDivTest() {
+        List<Integer> integers = Arrays.asList(1, 20, 300, 4000, 5000);
+        for (int i = 0; i < integers.size(); i++) {
+            mainPage.inputFieldLeft.setValue(integers.get(i).toString());
+            mainPage.inputFieldRight.setValue(integers.get(integers.size() - 1 - i).toString());
+            mainPage.keyDiv.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            float one = integers.get(i);
+            float two = integers.get(integers.size() - 1 - i);
+            String expected = String.format("%.2f", one / two);
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void floatsSumTest() {
+        List<Float> floats = Arrays.asList(0.1f, 0.02f, 0.3f, 1.4f, 2.5f);
+        for (int i = 0; i < floats.size(); i++) {
+            mainPage.inputFieldLeft.setValue(floats.get(i).toString());
+            mainPage.inputFieldRight.setValue(floats.get(floats.size() - 1 - i).toString());
+            mainPage.keyPlus.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            float one = floats.get(i);
+            float two = floats.get(floats.size() - 1 - i);
+            String expected = String.format("%.2f", one + two);
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void floatsMinusTest() {
+        List<Float> floats = Arrays.asList(0.1f, 0.02f, 0.3f, 1.4f, 2.5f);
+        for (int i = 0; i < floats.size(); i++) {
+            mainPage.inputFieldLeft.setValue(floats.get(i).toString());
+            mainPage.inputFieldRight.setValue(floats.get(floats.size() - 1 - i).toString());
+            mainPage.keyMinus.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            float one = floats.get(i);
+            float two = floats.get(floats.size() - 1 - i);
+            String expected = String.format("%.2f", one - two);
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void floatsMulTest() {
+        List<Float> floats = Arrays.asList(0.1f, 0.02f, 0.3f, 1.4f, 2.5f);
+        for (int i = 0; i < floats.size(); i++) {
+            mainPage.inputFieldLeft.setValue(floats.get(i).toString());
+            mainPage.inputFieldRight.setValue(floats.get(floats.size() - 1 - i).toString());
+            mainPage.keyMul.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            float one = floats.get(i);
+            float two = floats.get(floats.size() - 1 - i);
+            String expected = String.format("%.2f", one * two);
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void floatsDivTest() {
+        List<Float> floats = Arrays.asList(0.1f, 0.02f, 0.3f, 1.4f, 2.5f);
+        for (int i = 0; i < floats.size(); i++) {
+            mainPage.inputFieldLeft.setValue(floats.get(i).toString());
+            mainPage.inputFieldRight.setValue(floats.get(floats.size() - 1 - i).toString());
+            mainPage.keyDiv.click();
+            String v = mainPage.result.getText();
+            String[] res = v.split(" ");
+            v = res[res.length - 1];
+            float one = floats.get(i);
+            float two = floats.get(floats.size() - 1 - i);
+            String expected = String.format("%.2f", one / two);
+            Assert.assertEquals(expected, v);
+            reset();
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() {
         driver.quit();
     }
 }
